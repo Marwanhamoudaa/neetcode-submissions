72ac19cdbc363e6d88e5b06be31acba65022706c
Marwanhamoudaa <137093840+Marwanhamoudaa@users.noreply.github.com>
2026-07-02 15:15:31 +0300

    Add: buy-and-sell-crypto - submission-17

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-17.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+       let left = 0 
+       let right = 0 
+       let max = 0 
+       while (right < prices.length) {
+        if (prices[left] < prices[right]) { 
+            max = Math.max(max , prices[right] - prices[left] )
+        }else{
+            left = right
+        }
+
+        right ++ ; 
+       }
+       return max
+
+    }
+}
