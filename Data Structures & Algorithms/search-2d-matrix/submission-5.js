ec8860be1e1b29a2663e24c59b1a7802b9a21705
Marwanhamoudaa <137093840+Marwanhamoudaa@users.noreply.github.com>
2026-07-02 17:05:23 +0300

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        return new Set(matrix.flat()).has(target)
+
+    }
+}
