4c1e24996715fb2efb95c84a4709e8f40a366366
Marwanhamoudaa <137093840+Marwanhamoudaa@users.noreply.github.com>
2026-07-19 11:27:01 +0300

    Add: duplicate-integer - submission-26

---
+++ Data Structures & Algorithms/duplicate-integer/submission-26.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+     
+     const setNums = new Set (nums)
+     return setNums.size !== nums.length
+    
+    }
+
+
+}
